13c10abf99a89f4707ba7a45ee085d272c92f1c8
Andreanapolicy <daa.and@mail.ru>
2022-09-10 16:56:22 +0300

    Lab1. Добавил проверки для треугольника. Дополнительно поправил сообщения, который выбрасывались в валидации параметров

--- lab1/src/triangle.py
+++ lab1/src/triangle.py
@@ -9,17 +9,17 @@ EQUILATERAL_TRIANGLE = 'равносторонний'
 def validateArgs(args: list[str]) -> None:
     def checkArgsCount() -> None:
         if len(args) != 3:
-            raise ValueError(NOT_TRIANGLE_ERROR)
+            raise ValueError(UNDEFINED_ERROR)
 
     def checkArgsType() -> None:
         for element in args:
             if not element.isdigit():
-                raise ValueError(NOT_TRIANGLE_ERROR)
+                raise ValueError(UNDEFINED_ERROR)
 
     def checkArgsValue() -> None:
         for element in args:
             if int(element) < 0:
-                raise ValueError(NOT_TRIANGLE_ERROR)
+                raise ValueError(UNDEFINED_ERROR)
 
 
     checkArgsCount()
@@ -28,11 +28,34 @@ def validateArgs(args: list[str]) -> None:
     checkArgsValue()
 
 
+def getTriangleType(firstLine: int, secondLine: int, thirdLine: int) -> str:
+    def isTriangle() -> bool:
+        return firstLine + secondLine > thirdLine or thirdLine + secondLine > firstLine or firstLine + thirdLine > secondLine
+
+    def isIsoscelesTriangle() -> bool:
+        return firstLine == secondLine or thirdLine == secondLine or firstLine == thirdLine
+
+    def isEquilateralTriangle() -> bool:
+        return firstLine == secondLine and thirdLine == secondLine
+
+
+    if not isTriangle():
+        return NOT_TRIANGLE_ERROR
+
+    if not isEquilateralTriangle():
+        return EQUILATERAL_TRIANGLE
+
+    if not isIsoscelesTriangle():
+        return ISOSCELES_TRIANGLE
+
+    return USUAL_TRIANGLE
+
+
 try:
     args = sys.argv
     args.pop(0)
     validateArgs(args)
-    print('Ok, maybe its triangle, man')
+    print(getTriangleType(int(args[0]), int(args[1]), int(args[2])))
 except ValueError as error:
     print(error)
 except Exception:
