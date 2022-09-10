b54e2631b6b447380889a9974a2967c3778a766e
Andreanapolicy <daa.and@mail.ru>
2022-09-10 12:37:18 +0300

    Lab1. Сделал парс параметров, которые пришли на вход в программу

--- lab1/src/triangle.py
+++ lab1/src/triangle.py
@@ -2,11 +2,30 @@ import sys
 
 NOT_TRIANGLE_ERROR = 'не треугольник'
 
-def validateArgs(args):
-    if len(args) != 4:
-        raise ValueError(NOT_TRIANGLE_ERROR)
+def validateArgs(args: list[str]) -> None:
+    def checkArgsCount() -> None:
+        if len(args) != 3:
+            raise ValueError(NOT_TRIANGLE_ERROR)
+
+    def checkArgsType() -> None:
+        for element in args:
+            if not element.isdigit():
+                raise ValueError(NOT_TRIANGLE_ERROR)
+
+    def checkArgsValue() -> None:
+        for element in args:
+            if element < 0:
+                raise ValueError(NOT_TRIANGLE_ERROR)
+
+
+    checkArgsCount()
+    checkArgsType()
+    args = [int(element) for element in args]
+    checkArgsValue()
+
 
 try:
+    args = sys.argv.pop(0)
     validateArgs(sys.argv)
     print('Ok, maybe its triangle, man')
 except Exception as error:
