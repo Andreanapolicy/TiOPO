dd344ea4f71c3eb57a02718f974aa1f3162cd95d
Andreanapolicy <daa.and@mail.ru>
2022-09-25 18:14:00 +0300

    Lab2. Получил все ссылки со страницы

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 vgm_url = 'http://links.qatl.ru/'
 html_text = requests.get(vgm_url).text
 soup = BeautifulSoup(html_text, 'html.parser')
 
-print(soup.getText())
+needfulLinks = []
+
+for links in soup.findAll('a', href=True):
+    needfulLinks.append(links.get('href'))
+
+print(needfulLinks, '\n')
