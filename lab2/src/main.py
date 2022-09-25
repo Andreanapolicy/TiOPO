1480f7974b262250bdcd55ce1fb4cc0771ac15e2
Andreanapolicy <daa.and@mail.ru>
2022-09-25 18:16:36 +0300

    Lab2. Рефакторинг

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -1,13 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-vgm_url = 'http://links.qatl.ru/'
-html_text = requests.get(vgm_url).text
-soup = BeautifulSoup(html_text, 'html.parser')
+URL = 'http://links.qatl.ru/'
 
-needfulLinks = []
+def getHtmlDocument(url):
+    html_text = requests.get(url).text
+    return BeautifulSoup(html_text, 'html.parser')
 
-for links in soup.findAll('a', href=True):
-    needfulLinks.append(links.get('href'))
+def getAllLinksFromDocument(document):
+    needfulLinks = []
 
-print(needfulLinks, '\n')
+    for links in document.findAll('a', href=True):
+        needfulLinks.append(links.get('href'))
+
+    return needfulLinks
+
+print(getAllLinksFromDocument(getHtmlDocument(URL)))
