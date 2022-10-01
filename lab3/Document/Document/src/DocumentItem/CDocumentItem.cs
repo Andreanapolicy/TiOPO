4856732d962295cfabe451b5e06b432e259f2c6b
Andreanapolicy <daa.and@mail.ru>
2022-10-01 12:38:39 +0300

    Lab3. Реализовал класс, отвечающий за элемент документа

---
+++ lab3/Document/Document/src/DocumentItem/CDocumentItem.cs
@@ -0,0 +1,19 @@
+﻿using Document.src.Paragraph;
+
+namespace Document.src.DocumentItem
+{
+    class CDocumentItem : IDocumentItem
+    {
+        public CDocumentItem(IParagraph item)
+        {
+            m_item = item;
+        }
+
+        public IParagraph GetItem()
+        {
+            return m_item;
+        }
+
+        private IParagraph m_item;
+    }
+}
