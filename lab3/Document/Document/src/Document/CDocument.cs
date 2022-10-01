917d9cf069dd6b12ed0c7f358e4631f42f6e5331
Andreanapolicy <daa.and@mail.ru>
2022-10-01 13:29:42 +0300

    Lab3. Убрал часть методов из интерфейса документа и начал реализовывать его

---
+++ lab3/Document/Document/src/Document/CDocument.cs
@@ -0,0 +1,73 @@
+﻿using Document.src.DocumentItem;
+using Document.src.History;
+using Document.src.Paragraph;
+
+namespace Document.src.Document
+{
+    class CDocument : IDocument
+    {
+        public CDocument(IHistory history)
+        {
+            m_history = history;
+        }
+
+        public IDocumentItem GetItem(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetItemsCount()
+        {
+            return m_items.Count;
+        }
+
+        public string GetTitle()
+        {
+            return m_title;
+        }
+
+        public IParagraph InsertParagraph(string text, int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveItem(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReplaceParagraphText(int index, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanRedo()
+        {
+            return m_history.CanRedo();
+        }
+
+        public bool CanUndo()
+        {
+            return m_history.CanUndo();
+        }
+
+        public void Undo()
+        {
+            m_history.Undo();
+        }
+
+        public void Redo()
+        {
+            m_history.Redo();
+        }
+
+        private IHistory m_history;
+        private string m_title = "";
+        private List<IDocumentItem> m_items = new List<IDocumentItem>();
+    }
+}
