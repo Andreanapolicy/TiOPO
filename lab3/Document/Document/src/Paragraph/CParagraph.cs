6e74ecd77271b2bf8ea5c73d67320b6c83654b62
Andreanapolicy <daa.and@mail.ru>
2022-10-01 12:44:02 +0300

    Lab3. Реализовал логику параграфа. Внедрил в Параграф историю и команду на апдейт текста. Реализация команд и истории команд будет позже

---
+++ lab3/Document/Document/src/Paragraph/CParagraph.cs
@@ -0,0 +1,24 @@
+﻿namespace Document.src.Paragraph
+{
+    class CParagraph : IParagraph
+    {
+        CParagraph(string text, IHistory history)
+        {
+            m_text = text;
+            m_history = history;
+        }
+
+        public string GetText()
+        {
+            return m_text;
+        }
+
+        public void SetText(string text)
+        {
+            m_history.AddAndExecuteCommand(new CReplaceTextParagraphCommand(m_text, text));
+        }
+
+        private string m_text;
+        private IHistory m_history;
+    }
+}
