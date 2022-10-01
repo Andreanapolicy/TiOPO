f8a2607164e16b50000f73908cdddd65081b221e
Andreanapolicy <daa.and@mail.ru>
2022-10-01 13:05:56 +0300

    Lab3. Написал команду для обновления текста

---
+++ lab3/Document/Document/src/Command/CReplaceTextParagraphCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace Document.src.Command
+{
+    class CReplaceTextParagraphCommand : ICommand
+    {
+        public CReplaceTextParagraphCommand(ref string currentText, ref string newText)
+        {
+            m_currentText = currentText;
+            m_newText = newText;
+        }
+
+        public void Execute()
+        {
+            (m_currentText, m_newText) = (m_newText, m_currentText);
+        }
+
+        public void Unexecute()
+        {
+            (m_currentText, m_newText) = (m_newText, m_currentText);
+        }
+
+        private string m_currentText;
+        private string m_newText;
+    }
+}
