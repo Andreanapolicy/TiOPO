b8f3cbe4e243947487096f8949291a31ca52265c
Andreanapolicy <daa.and@mail.ru>
2022-10-02 12:31:35 +0300

    Lab3. Добавил тест на конструирование объекта

--- lab3/Document/DocumentTests/UnitTest1.cs
+++ lab3/Document/DocumentTests/UnitTest1.cs
@@ -8,10 +8,21 @@ namespace DocumentTests
     public class DocumentTests
     {
         [Test]
-        public void CreateDocument()
+        public void Check_Document_Constructing()
         {
             IHistory history = new CMockHistory();
             CDocument document = new CDocument(history);
+
+            Assert.IsNotNull(document);
+            Assert.AreEqual(document.GetTitle(), "");
+            Assert.AreEqual(document.GetItemsCount(), 0);
+            Assert.IsFalse(document.CanRedo());
+            Assert.IsFalse(document.CanUndo());
+        }
+
+        [Test]
+        public void Check_Document_Adding_Paragraph()
+        {
         }
     }
 }
