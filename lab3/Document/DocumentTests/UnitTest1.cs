d5bd7d4b6f6fabadb61dab042af5212d59f8de91
Andreanapolicy <daa.and@mail.ru>
2022-10-02 13:04:23 +0300

    Lab3. Написал тест по добавлению параграфа в нужную позицию. Дополнительно создал вспомогательную функцию по проверке истории

--- lab3/Document/DocumentTests/UnitTest1.cs
+++ lab3/Document/DocumentTests/UnitTest1.cs
@@ -7,22 +7,43 @@ namespace DocumentTests
 {
     public class DocumentTests
     {
+        public void checkHistoryState(ref IDocument document, bool isAvailableUndo, bool isAvailableRedo)
+        {
+            Assert.AreEqual(document.CanRedo(), isAvailableRedo);
+            Assert.AreEqual(document.CanUndo(), isAvailableUndo);
+        }
+
         [Test]
         public void Check_Document_Constructing()
         {
             IHistory history = new CMockHistory();
-            CDocument document = new CDocument(history);
+            IDocument document = new CDocument(history);
 
             Assert.IsNotNull(document);
             Assert.AreEqual(document.GetTitle(), "");
             Assert.AreEqual(document.GetItemsCount(), 0);
-            Assert.IsFalse(document.CanRedo());
-            Assert.IsFalse(document.CanUndo());
+            checkHistoryState(ref document, false, false);
         }
 
         [Test]
-        public void Check_Document_Adding_Paragraph()
+        public void Check_Document_Adding_Paragraph_Into_Wrong_Position()
         {
+            IHistory history = new CMockHistory();
+            CDocument document = new CDocument(history);
+
+            Assert.Catch(() => document.InsertParagraph("test", 1), "Index is out of range");
+        }
+
+        [Test]
+        public void Check_Document_Adding_Paragraph_Into_Right_Position()
+        {
+            IHistory history = new CMockHistory();
+            IDocument document = new CDocument(history);
+
+            Assert.DoesNotThrow(() => document.InsertParagraph("test", 0));
+
+            Assert.AreEqual(document.GetItemsCount(), 1);
+            checkHistoryState(ref document, true, false);
         }
     }
 }
