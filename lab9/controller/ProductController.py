e0a0afd415232b0bb8d5052fdb86354045f0cfa1
Andreanapolicy <daa.and@mail.ru>
2022-11-20 16:44:03 +0300

    Lab9. Добавил методы в контроллер

--- lab9/controller/ProductController.py
+++ lab9/controller/ProductController.py
@@ -6,3 +6,15 @@ class ProductController:
     def deleteById(self, id):
         response = Client.custom_request('GET', BASE_URL + API_PREFIX + DELETE_PRODUCT_REQUEST, id)
         return response
+
+    def create(self, params):
+        response = Client.custom_request('POST', BASE_URL + API_PREFIX + CREATE_PRODUCT_REQUEST, params)
+        return response
+
+    def edit(self, params):
+        response = Client.custom_request('POST', BASE_URL + API_PREFIX + EDIT_PRODUCT_REQUEST, params)
+        return response
+
+    def getAll(self):
+        response = Client.custom_request('GET', BASE_URL + API_PREFIX + GET_ALL_PRODUCTS_REQUEST)
+        return response
