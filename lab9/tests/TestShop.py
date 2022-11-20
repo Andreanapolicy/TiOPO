47e6d277ebf1853ceb05a8083ad8da33bf3cd146
Andreanapolicy <daa.and@mail.ru>
2022-11-20 16:37:39 +0300

    Lab9. Добавил tearDown

--- lab9/tests/TestShop.py
+++ lab9/tests/TestShop.py
@@ -4,25 +4,18 @@ from lab9.controller.ProductController import ProductController
 
 
 class TestShop(unittest.TestCase):
-    # def __init__(self):
-    #     self.productController = ProductController()
-    #     self.createdProducts = []
-
-    # @pytest.fixture(autouse=True)
-    # def beforeAndAfter(self):
-    #     print('just beforeAndAfter')
-    #     if len(self.createdProducts) > 0:
-    #         for createdProduct in self.createdProducts:
-    #             self.productController.deleteById(createdProduct)
-
     def setUp(self):
         self.productController = ProductController()
         self.createdProducts = []
+
+    def tearDown(self):
+        if len(self.createdProducts) > 0:
+            for createdProduct in self.createdProducts:
+                self.productController.deleteById(createdProduct)
+        self.productController = None
 
     def test_CreateProduct(self):
         assert len(self.createdProducts) == 0
 
     def test_deleteProductById(self):
         assert 1 == 1
-        #create priduct
-        #delete product
