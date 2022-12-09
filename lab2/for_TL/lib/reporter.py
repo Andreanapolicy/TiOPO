2cdc40d1601952b1781b7f99a91c47206edfc83c
Andreanapolicy <daa.and@mail.ru>
2022-12-10 00:26:14 +0300

    Lab2. Поправил названия

--- lab2/for_TL/lib/reporter.py
+++ lab2/for_TL/lib/reporter.py
@@ -2,24 +2,24 @@ import datetime
 
 
 def report(successFileName, failFileName, links):
-    with open(failFileName, "w") as file:
+    with open(failFileName, 'w') as file:
         total = 0
 
         for link in links:
             if link.status > 400:
-                file.write(link.PREVIOUS_URL + " --- " + link.url + " " + str(link.status))
+                file.write(link.PREVIOUS_URL + ' ---from--- ' + link.url + ' ' + '[' + str(link.status) + ']')
                 total += 1
 
-        file.write("Total links: " + str(total))
-        file.write("Precessed date: " + str(datetime.datetime.now()))
+        file.write('Links count processed: ' + str(total))
+        file.write('Precessed date: ' + str(datetime.datetime.now()))
 
-    with open(successFileName, "w") as file:
+    with open(successFileName, 'w') as file:
         total = 0
 
         for link in links:
             if link.status < 400:
-                file.write(link.url + " " + str(link.status))
+                file.write(link.url + ' ' + '[' + str(link.status) + ']')
                 total += 1
 
-        file.write("Total links: " + str(total))
-        file.write("Precessed date: " + str(datetime.datetime.now()))
+        file.write('Links count processed: ' + str(total))
+        file.write('Precessed date: ' + str(datetime.datetime.now()))
